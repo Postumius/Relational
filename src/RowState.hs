c23e046d9306893971bbad3d85720ca5ed629e2e
Noa Ma <noahfierz@gmail.com>
2024-05-10 11:42:03 -0700

    accumulation works, and I *think* it's even monad-legal

--- src/RowState.hs
+++ src/RowState.hs
@@ -1,7 +1,7 @@
 #!/usr/bin/env cabal
 {- cabal:
 ghc-options: -Wunused-imports
-build-depends: base, containers, mtl
+build-depends: base, containers, mtl, lens
 other-modules: Utilities
 -}
 
@@ -16,7 +16,6 @@ module RowState
 , whenExists
 , bind
 , release
-, exec
 ) where
 
 import Utilities
@@ -26,6 +25,8 @@ import qualified Data.Map.Strict as Map
 import Data.Functor
 import Control.Monad.State
 import Data.Either
+import Control.Lens
+import Data.List
 
 data Atom = Null | Number Int | Words String
   deriving (Eq, Ord)
@@ -53,79 +54,104 @@ wordsT =
       unwrapWords atom = wrongType atom "wordsT"
   in (unwrapWords, Words)
 
-newtype RowState a = RowState { runRS :: [IndexedRow] -> Either String [(a, IndexedRow)] }
+newtype RowState a b = RowState { runRS :: a -> IndexedRow -> Either String (a, [(b, IndexedRow)]) }
 
-instance Functor RowState where
-  fmap = (<$>)
+applyRS a r rs = runRS rs a r
 
-instance Applicative RowState where
-  pure x = RowState $ \rows -> zip (repeat x) rows & Right
+instance Functor (RowState a) where
+  fmap = (<*>) . pure
+
+instance Applicative (RowState a) where
+  pure x = RowState $ \a row -> Right (a, [(x, row)])
   (<*>) = ap
 
-instance Monad RowState where
-  (RowState h) >>= f = RowState $ \rows -> do
-    (as, rows') <- h rows <&> unzip
-    let gs = map (f .- runRS) as
-    zip gs rows'
-      & mapM (\(g, row) -> g [row])
-      <&> concat
+instance Monad (RowState a) where
+  (RowState h) >>= f = RowState $ \a row -> do
+    (a', pairs) <- h a row
+    foldM (\(aAcc, pairsAcc) (x, row') ->
+        f x & applyRS aAcc row' <&> over _2 (++pairsAcc)
+      ) (a', []) pairs
 
-getRow :: RowState IndexedRow
-getRow = RowState $ \rows -> zip rows rows & Right
+getRow :: RowState a IndexedRow
+getRow = RowState $ \a row -> Right (a, [(row, row)])
 
-putRow :: IndexedRow -> RowState ()
-putRow row = RowState $ \rows -> repeat ((), row) & take (length rows) & Right
+--putRow :: IndexedRow -> RowState a ()
+--putRow row = RowState $ \a _ -> Right (a, [((), row)])
 
-modifyRow :: (IndexedRow -> IndexedRow) -> RowState ()
-modifyRow f = getRow >>= (f .- putRow)
-
-deleteRow :: RowState ()
-deleteRow = RowState $ const [] .- Right
-
-bring :: [IndexedRow] -> RowState ()
-bring otherRows = RowState $ \rows ->
-  Map.union <$> rows <*> otherRows
+putRows :: [IndexedRow] -> RowState a ()
+putRows rows = RowState $ \a _ -> 
+  rows
   & zip (repeat ())
+  & ((,) a)
   & Right
 
-liftEither :: Either String a -> RowState a
-liftEither (Right a) = return a
-liftEither (Left e) = RowState $ const (Left e)
+putRow = singleton .- putRows
 
-access :: ColType a -> String -> RowState a
+assembleModify g p f = g <&> f >>= p
+
+modifyRow :: (IndexedRow -> IndexedRow) -> RowState a ()
+modifyRow = assembleModify getRow putRow
+
+multiplyRow :: (IndexedRow -> [IndexedRow]) -> RowState a ()
+multiplyRow = assembleModify getRow putRows
+
+deleteRow :: RowState a ()
+deleteRow = putRows []
+
+getAcc = RowState $ \acc row -> Right (acc, [(acc, row)])
+
+putAcc acc = RowState $ \_ row -> Right (acc, [((), row)])
+
+modifyAcc = assembleModify getAcc putAcc
+
+bringIn :: [IndexedRow] -> RowState a ()
+bringIn otherRows = multiplyRow $ \row -> map (Map.union row) otherRows
+
+liftEither :: Either String b -> RowState a b
+liftEither (Right x) = return x
+liftEither (Left e) = RowState $ \_ _ -> Left e
+
+access :: ColType b -> String -> RowState a b
 access (unwrap, _) key = do
   let errMsg = "field '"++ key ++"' not found"
   row <- getRow
   atom <- Map.lookup key row & explain errMsg & liftEither
   atom & unwrap & liftEither
 
-whenExists :: ColType a -> String -> RowState () -> RowState ()
+whenExists :: ColType a -> String -> RowState a () -> RowState a ()
 whenExists (unwrap, _) key rowOp = do
   row <- getRow
   let exists = (Map.lookup key row & explain "") >>= unwrap & isRight
   when exists rowOp
 
-bind :: ColType a -> String -> a -> RowState ()
+bind :: ColType a -> String -> a -> RowState a ()
 bind (_, wrap) key val = Map.insert key (wrap val) & modifyRow
 
-release :: String -> RowState ()
+release :: String -> RowState a ()
 release = Map.delete .- modifyRow
 
-keepOnly :: [String] -> RowState ()
+keepOnly :: [String] -> RowState a ()
 keepOnly fields = zip fields (repeat ())
   & Map.fromList 
   & flip Map.intersection 
   & modifyRow
 
-exec :: RowState a -> [IndexedRow] -> Either String [IndexedRow]
-exec rowState rows = runRS rowState rows <&> map snd
+applyAccum :: a -> [IndexedRow] -> RowState a b -> Either String (a, [b], [IndexedRow])
+applyAccum acc rows rs = 
+  putRows rows >> rs 
+  & applyRS acc Map.empty
+  <&> over _2 (reverse .- unzip)
+  <&> (\(acc', (xs, rows)) -> (acc', xs, rows))
 
-row1 = Map.fromList [("n",Number 1),("m",Number 2)]
+row1 = Map.fromList [("a",Number 1),("b",Number 2)]
 
-row2 = Map.fromList [("a",Number 1),("b",Number 2)]
+row2 = Map.fromList [("n",Number 1),("m",Number 2)]
 
 row3 = Map.fromList [("x",Number 1),("y",Number 2)]
 
 testRS = do
   row <- getRow
   when ("n" `Map.member` row) $ modifyRow (Map.insert "u" $ Words "bla")
+  bringIn [row1, row2, row3]
+  getAcc >>= bind numberT "i"
+  modifyAcc (+1)
